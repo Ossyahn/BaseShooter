d928fd847458980f7bc44c23fcc1ce5834504a8c
Nacapa <nacapa.tgn@gmail.com>
2020-05-30 14:54:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Infinite Shooter
+
+An FPS prototype with procedurally generated rooms and common FPS features.
+
+Developed with Unreal Engine 4
---
+++ Source/InfiniteShooter/InfiniteShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfiniteShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInfiniteShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInfiniteShooterGameMode();
+};
+
+
+
