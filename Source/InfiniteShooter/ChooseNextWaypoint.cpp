07a389d31bd9d942fccbb30c139277ed5063d0ee
Nacapa <nacapa.tgn@gmail.com>
2020-05-30 18:26:28 -0700

    Test ChooseNextWaypoint

--- Source/InfiniteShooter/ChooseNextWaypoint.cpp
+++ Source/InfiniteShooter/ChooseNextWaypoint.cpp
@@ -2,9 +2,15 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
-{
-	UE_LOG(LogTemp, Warning, TEXT("ChooseNextWaypoint task executed"));
+{	
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	int IndexVal = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), IndexVal);
+
+
 	return EBTNodeResult::Succeeded;
 }
