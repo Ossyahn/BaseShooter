e66256ed9ae1e39e95d24c223e5f2e1e66b53b2a
Nacapa <nacapa.tgn@gmail.com>
2020-06-01 14:19:15 -0700

    Fix PatrolRoute behavior

--- Source/InfiniteShooter/ChooseNextWaypoint.cpp
+++ Source/InfiniteShooter/ChooseNextWaypoint.cpp
@@ -4,28 +4,29 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
-{	
-	auto Blackboard = OwnerComp.GetBlackboardComponent();
-	if (!Blackboard) return EBTNodeResult::Failed;
-
+{
 	auto Pawn = OwnerComp.GetAIOwner()->GetPawn();
-	auto Guard = Cast<APatrollingGuard>(Pawn);
+	auto PatrolRouteComponent = Pawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRouteComponent)) return EBTNodeResult::Failed;
 
-	TArray<AActor*> Waypoints = Guard->Waypoints;
+	TArray<AActor*> Waypoints = PatrolRouteComponent->GetWaypoints();
 	if (Waypoints.Num() == 0) return EBTNodeResult::Failed;
+
+	auto Blackboard = OwnerComp.GetBlackboardComponent();
+	if (!ensure(Blackboard)) return EBTNodeResult::Failed;
 
 	int Index = Blackboard->GetValueAsInt(IndexKey.SelectedKeyName);
 	auto NextWaypoint = Waypoints[Index];
+	
+	Blackboard->SetValueAsObject(WaypointKey.SelectedKeyName, NextWaypoint);
 
 	Index++;
 	Index %= Waypoints.Num();
-
 	Blackboard->SetValueAsInt(IndexKey.SelectedKeyName, Index);
-	Blackboard->SetValueAsObject(WaypointKey.SelectedKeyName, NextWaypoint);
 
 	return EBTNodeResult::Succeeded;
 }
--- Source/InfiniteShooter/PatrolRoute.cpp
+++ Source/InfiniteShooter/PatrolRoute.cpp
@@ -6,13 +6,13 @@
 // Sets default values for this component's properties
 UPatrolRoute::UPatrolRoute()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	//PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
+
+TArray<AActor*> UPatrolRoute::GetWaypoints()
+{
+	return Waypoints;
+}
 
 // Called when the game starts
 void UPatrolRoute::BeginPlay()
