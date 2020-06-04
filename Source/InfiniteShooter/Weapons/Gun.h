7df5f7b3e5c7f5284fb8dcc26589878a6252d40a
Nacapa <nacapa.tgn@gmail.com>
2020-06-04 11:43:47 -0700

    Create Gun class

---
+++ Source/InfiniteShooter/Weapons/Gun.h
@@ -0,0 +1,26 @@
+// Copyright 2020 Alberto Romero Garcia, all rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class INFINITESHOOTER_API AGun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
