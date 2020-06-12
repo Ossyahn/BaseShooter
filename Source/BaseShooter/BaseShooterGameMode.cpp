894047f8a142c18c3a005488c062e815074a62c3
Alberto <nacapa.tgn@gmail.com>
2020-06-12 11:45:08 -0700

    Create game new base game mode blueprint and use as default instead of c++ game mode

--- Source/BaseShooter/BaseShooterGameMode.cpp
+++ Source/BaseShooter/BaseShooterGameMode.cpp
@@ -9,7 +9,7 @@ ABaseShooterGameMode::ABaseShooterGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BaseShooter/Characters/Common/BP_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BaseShooter/Character/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
